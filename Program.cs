0e565fab05bb3a5664a8fc608c1cf4d3360953d4
Mateja97 <matejarkvc@gmail.com>
2021-01-22 18:16:33 +0100

    updated

--- Program.cs
+++ Program.cs
@@ -7,86 +7,30 @@ using System.Net;
 using System.Text;
 using System.Linq;
 using System.Collections.Generic;
+using System.Threading;
+
 namespace web_scrap
 {
     
         class Program
     {
-
         static void Main(string[] args)
         {
+            System.Net.ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
 
-           
             var options = getCurrencyOptions();
-    
-            Console.WriteLine("Type currency you want:  ");
-            var currencyInput = Console.ReadLine();
-            while(!options.Contains(currencyInput)){
+            options = options.Where(o => o.Length == 3);
 
-                Console.WriteLine("Wrong currency:  ");
-                currencyInput = Console.ReadLine();
 
+            foreach(var opt in options){
+               Thread thread = new Thread(() => scrapeCurrency(opt));
+               thread.Start();
             }
             
-            DateTime dateTime = DateTime.UtcNow.Date;
-            var curr_date =  dateTime.ToString("yyyy-MM-dd");
-            var first_date = dateTime.AddDays(-2).ToString("yyyy-MM-dd");
-
-            string result = goToPage(currencyInput,1,first_date,curr_date);
-            
-            HtmlDocument doc = new HtmlDocument();
-            doc.LoadHtml(result);
-        
-            var html = doc.DocumentNode;
-
-            
-
-            using (var sr = new StreamWriter(currencyInput+"-"+first_date+"-"+curr_date+".csv")) {
-            using (var csv = new CsvWriter(sr,System.Globalization.CultureInfo.CurrentCulture)) {
-                csv.WriteField("CurrencyName");
-                csv.WriteField("BuyingRate");
-                csv.WriteField("CashBuyingRate");
-                csv.WriteField("SellingRate");
-                csv.WriteField("CashSellingRate");
-                csv.WriteField("MiddleRate");
-                csv.WriteField("PubTime");
-                csv.NextRecord();
-                int count = 2;
-                var pom = result;
-                while(true){
-                    var trs = html.CssSelect("tr");
-                    foreach (var tr in trs)
-                    {
-
-                        var tds = tr.CssSelect("td");
-                        
-                        foreach(var td in tds){
-
-                            if (td.GetAttributeValue("class").Equals("hui12_20"))
-                            {
-                                csv.WriteField(td.InnerText);
-                            }
-                        }
-                        csv.NextRecord();
-                        sr.Flush();               
-                    }
-
-                   result = goToPage(currencyInput,count,first_date,curr_date);
-                    
-                    
-                    if(result.Equals(pom)){
-                       break;
-                    }
-
-                    Console.WriteLine("count of scraped pages: " +count);
-                    pom = result;
-                    count++;
-                }
-              }
-            }
         }
         static string goToPage(String currencyInput,int count,string first_date, string curr_date){
-              HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://srh.bankofchina.com/search/whpj/searchen.jsp");
+            try{
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://srh.bankofchina.com/search/whpj/searchen.jsp");
             request.Method = WebRequestMethods.Http.Post;
             request.ContentType = "application/x-www-form-urlencoded";
 
@@ -108,15 +52,85 @@ namespace web_scrap
             result = reader.ReadToEnd();
             newStream.Close();
             return result;
+             }
+            catch(Exception ex){
+                Console.WriteLine(ex.Message);
+                return null;
+            }
         }
 
         public static IEnumerable<string> getCurrencyOptions(){
 
-             HtmlWeb web = new HtmlWeb();
+            HtmlWeb web = new HtmlWeb();
             var htmlDoc = web.Load("https://srh.bankofchina.com/search/whpj/searchen.jsp");
             var options = htmlDoc.GetElementbyId("pjname").CssSelect("option").Select(l => l.InnerText);
             return options;
 
+        }
+        public static void scrapeCurrency(string currencyInput){
+
+
+            DateTime dateTime = DateTime.UtcNow.Date;
+            var curr_date =  dateTime.ToString("yyyy-MM-dd");
+            var first_date = dateTime.AddDays(-2).ToString("yyyy-MM-dd");
+            string result = goToPage(currencyInput,1,first_date,curr_date);
+            
+            try{
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(result);       
+            var html = doc.DocumentNode;
+
+            using (var sr = new StreamWriter(currencyInput+"-"+first_date+"-"+curr_date+".csv")) {
+            using (var csv = new CsvWriter(sr,System.Globalization.CultureInfo.CurrentCulture)) {
+                csv.WriteField("CurrencyName");
+                csv.WriteField("BuyingRate");
+                csv.WriteField("CashBuyingRate");
+                csv.WriteField("SellingRate");
+                csv.WriteField("CashSellingRate");
+                csv.WriteField("MiddleRate");
+                csv.WriteField("PubTime");
+                csv.NextRecord();
+                int count = 2;
+                var pom = result;
+                while(true){
+                    var trs = html.CssSelect("tr");
+                    if(trs.Count()<7){
+                        break;
+                    }
+                    foreach (var tr in trs)
+                    {
+
+                        var tds = tr.CssSelect("td");
+                        
+                        foreach(var td in tds){
+
+                            if (td.GetAttributeValue("class").Equals("hui12_20"))
+                            {
+                                csv.WriteField(td.InnerText);
+                            }
+                            
+                        }
+                        csv.NextRecord();
+                        sr.Flush();               
+                    }
+
+                   result = goToPage(currencyInput,count,first_date,curr_date);
+                    
+                    
+                    if(result.Equals(pom)){
+                       break;
+                    }
+
+                    Console.WriteLine("count of scraped pages: " +count + " of: " + currencyInput );
+                    pom = result;
+                    count++;
+                }
+              }
+            }
+
+            }catch(Exception ex){
+                Console.WriteLine(ex.Message);
+            }
         }
   
     }
